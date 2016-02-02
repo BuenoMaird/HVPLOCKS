3b8385750f374d5a9667a437e5fb4ca9cd07663f
CIT Student <bengrace101@gmail.com>
2016-02-02 11:59:20 +1100

    first

---
+++ index.php
@@ -0,0 +1,146 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Precision Locksmithing</title>
+  <link rel="stylesheet" href="css/styles.css">
+  <link rel="stylesheet" href="fonts/bebas_neue/stylesheet.css">
+</head>
+<body>  
+
+<header id="header">
+  <div id="head_content">
+
+    <div class="head_left">
+      <div class="head_logo">
+        <a href="#header"><img src="images/head_text.svg" alt=""></a>
+      </div>
+      <div class="head_left_text">
+        <h1>24/7 Locksmithing services</h1>
+      </div>
+    </div> 
+    <!-- End head left content -->
+
+    <div class="head_right">
+      <h1 id="head_email">admin@hvplocksmiths.com</h1>
+      <h1 id="head_phone">
+        0404 785 921
+      </h1>
+    </div>
+  </div>
+</header>
+
+<nav>
+  <div id="nav_top">
+    <a href="#commercial"><div class="nav_button">
+      <h1>Commercial</h1>
+    </div></a>
+    <a href="#domestic"><div class="nav_button">
+      <h1>Domestic</h1>
+    </div></a>
+  </div>
+  <div id="nav_bottom">
+    <a href="#automotive"><div class="nav_button">
+      <h1>Automotive</h1>
+    </div></a>
+    <a href="#contact"><div class="nav_button">
+      <h1>Contact</h1>
+    </div></a>
+  </div>
+</nav>
+
+<section id="about">
+  <h1>Hunter Valley Precision Locksmiths</h1>
+  <p>
+    This part will be filled with a blurb about you and your company. I need something to throw in here because I can’t really think of anything so far unfortunately.</p>
+  <p>Maybe something along the lines of...
+  Hunter Valley Precision Locksmiths is dedicated to securing the Newcastle and Greater Hunter Valley region with exceptional 24 / 7 locksmithing services ranging from Commerical to Domestic and Automtive.
+  </p>
+  <p>Please continue to scroll for a full outline of services or click the links above.</p>
+</section>
+
+<section id="commercial">
+
+  <img src="images/Commercial.png" alt="">
+  <div class="main_content">
+    <div class="mcl">
+      <h1>Our commercial locksmiths</h1>
+
+      <p>Whether it’s a pre existing building or still in construction, we can provide any security requirement you need. Our Locksmiths have full security accreditation, carry all construction certificates needed and are OHS trained.</p>
+    </div>
+
+    <div class="mcr">
+    <h1>Our Commercial Services</h1>
+      <ul>
+        <li>Electric Strike Installation</li>
+        <li>Lock fitting</li>
+        <li>Hardware</li>
+        <li>Routine Maintenance</li>
+      </ul>
+    </div>
+  </div>
+
+</section>
+
+<section id="domestic">
+  <div class="main_content">
+    <div class="mcr">
+      <h1>Our Domestic Services</h1>
+      <ul>
+        <li>Lockouts</li>
+        <li>Rekeying on premesis</li>
+        <li>Lock installation</li>
+        <li>Security door repairs</li>
+        <li>Key cutting</li>
+        <li>Security Upgrades</li>
+      </ul>
+    </div>
+    <div class="mcl">
+      <h1>Our Domestic Locksmiths</h1>
+      <p>Our dedicated Master Locksmiths are available 24/7 for all your security needs.</p>
+
+      <p> We take pleasure in assisting the Hunter Valley and surrounds with security services. Be it from opening your home when keys are locked inside, to ensuring every lock in your home works on a single key, we’re here to help.
+      </p>
+    </div>
+  </div>
+</section>
+
+<section id="automotive">
+  
+<img src="images/automotive.png" alt="">
+  <div class="main_content">
+    <div class="mcl">
+      <h1>Our Automotive locksmiths</h1>
+
+      <p>Keys locked in your car or they just won’t work? We’re here to help 24/7. Our fully mobile locksmiths can meet you at your car to assist you.</p>
+    </div>
+
+    <div class="mcr">
+    <h1>Our Automotive Services</h1>
+      <ul>
+        <li>Car opening</li>
+        <li>Lock replacement</li>
+        <li>Ignition and doorlock repair</li>
+      </ul>
+    </div>
+  </div>
+
+</section>
+<section id="contact">
+  <div class="mcl">
+    <h1>Contact us via:</h1>
+    <p>For more information, quotes or quick and quality service please contact us on</p>
+  </div>
+
+  <div class="mcr">
+    <h2>Phone: 0404 785 921</h2>
+    <h2>Email: Admin@HVPLOCKS.com.au</h2>
+  </div>
+
+</section>
+<section></section>
+
+<footer></footer>
+  
+</body>
+</html>
